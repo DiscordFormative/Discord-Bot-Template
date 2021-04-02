7e92489439badc5d883fa25f348add843f75a839
Software Developer <62594652+DiscordFormative@users.noreply.github.com>
2021-04-02 12:00:18 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    app: {
+        token: 'ODIwMzcxMjU3OTc5NDM3MDc3.YE0MRQ.7P_gyf99xnhYFJRiod5B3m9Obts'
+    },
+
+    data: {
+        prefix: '!',
+        activity: '!help'
+    }
+};
